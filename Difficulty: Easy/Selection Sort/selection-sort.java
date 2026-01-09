4c24dfd68ab558f42a900f951647335984d93895
Suraj Kumar <162699747+surajkum7462@users.noreply.github.com>
2026-01-09 19:38:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.java
@@ -0,0 +1,17 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        
+        for(int i=1;i<arr.length;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && key<arr[j])
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+}
